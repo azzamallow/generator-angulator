0fa6b43736a79b0d29ccd3f8098d2264a173660b
azzamallow <azzamallow@gmail.com>
2013-12-12 20:42:53 -0500

    ask questions, limit to only directive right now

--- app/index.js
+++ app/index.js
@@ -2,29 +2,29 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-
+var spawn = require('child_process').spawn;
 
 var AngularComponentGenerator = module.exports = function AngularComponentGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
     this.installDependencies({ skipInstall: options['skip-install'] });
-  });
 
-  this.invoke('karma:app', {
-    options: {
-      coffee: true,
-      travis: true,
-      'skip-install': this.options['skip-install'],
-      components: [
-        'angular/angular.js',
-        'angular-resource/angular-resource.js',
-        'angular-cookies/angular-cookies.js',
-        'angular-sanitize/angular-sanitize.js',
-        'angular-route/angular-route.js',
-        'angular-mocks/angular-mocks.js'
-      ]
-    }
+    this.invoke('karma:app', {
+      options: {
+        coffee: true,
+        travis: true,
+        'skip-install': this.options['skip-install'],
+        components: [
+          'angular/angular.js',
+          'angular-resource/angular-resource.js',
+          'angular-cookies/angular-cookies.js',
+          'angular-sanitize/angular-sanitize.js',
+          'angular-route/angular-route.js',
+          'angular-mocks/angular-mocks.js'
+        ]
+      }
+    });
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
@@ -32,16 +32,42 @@ var AngularComponentGenerator = module.exports = function AngularComponentGenera
 
 util.inherits(AngularComponentGenerator, yeoman.generators.Base);
 
+AngularComponentGenerator.prototype.askForType = function askForType() {
+  var cb = this.async();
+
+  this.prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of your component?'
+  },{
+    type: 'list',
+    name: 'type',
+    message: 'What type of component would you like to create?',
+    choices: ['directive'],
+    default: 'directive'
+  }], function (props) {
+    this.env.options.name = props.name;
+    this.env.options.type = props.type;
+
+    cb();
+  }.bind(this));
+};
+
 AngularComponentGenerator.prototype.app = function app() {
   this.mkdir('app');
   this.mkdir('app/scripts');
-  this.copy('scripts/directive.coffee', 'app/scripts/directive.coffee');
 };
 
 AngularComponentGenerator.prototype.test = function test() {
   this.mkdir('test');
   this.mkdir('test/spec');
-  this.copy('test/directive.coffee', 'test/spec/directive.coffee');
+};
+
+AngularComponentGenerator.prototype.component = function component() {
+  if (this.env.options.type === 'directive') {
+    this.copy('scripts/directive.coffee', 'app/scripts/' + this.env.options.name + '.coffee');
+    this.copy('test/directive.coffee',    'test/spec/' + this.env.options.name + '.coffee');
+  }
 };
 
 AngularComponentGenerator.prototype.packageFiles = function packageFiles() {
